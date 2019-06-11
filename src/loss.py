8e8fd47bf81e283304051dfebd3861ee885dd849
chenshen03 <675121663@qq.com>
2019-06-11 12:45:11 +0800

    初始化

---
+++ src/loss.py
@@ -0,0 +1,72 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+def pairwise_loss(outputs1, outputs2, label1, label2, sigmoid_param=1.0, l_threshold=15.0, class_num=1.0):
+    similarity = Variable(torch.mm(label1.data.float(), label2.data.float().t()) > 0).float()
+    dot_product = sigmoid_param * torch.mm(outputs1, outputs2.t())
+    exp_product = torch.exp(dot_product)
+    mask_dot = dot_product.data > l_threshold
+    mask_exp = dot_product.data <= l_threshold
+    mask_positive = similarity.data > 0
+    mask_negative = similarity.data <= 0
+    mask_dp = mask_dot & mask_positive
+    mask_dn = mask_dot & mask_negative
+    mask_ep = mask_exp & mask_positive
+    mask_en = mask_exp & mask_negative
+
+    dot_loss = dot_product * (1-similarity)
+    exp_loss = (torch.log(1+exp_product) - similarity * dot_product)
+    loss = (torch.sum(torch.masked_select(exp_loss, Variable(mask_ep))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dp)))) * class_num + torch.sum(torch.masked_select(exp_loss, Variable(mask_en))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dn)))
+
+    return loss / (torch.sum(mask_positive.float()) * class_num + torch.sum(mask_negative.float()))
+
+
+def pairwise_loss_origin(outputs1, outputs2, label1, label2):
+    '''https://github.com/thuml/HashNet/issues/17#issuecomment-443137529'''
+    similarity = Variable(torch.mm(label1.data.float(), label2.data.float().t()) > 0).float()
+    dot_product = torch.mm(outputs1, outputs2.t())
+    #exp_product = torch.exp(dot_product)
+
+    mask_positive = similarity.data > 0
+    mask_negative = similarity.data <= 0
+    exp_loss = torch.log(1+torch.exp(-torch.abs(dot_product))) + torch.max(dot_product, Variable(torch.FloatTensor([0.]).cuda()))-similarity * dot_product
+    #weight
+    S1 = torch.sum(mask_positive.float())
+    S0 = torch.sum(mask_negative.float())
+    S = S0+S1
+    exp_loss[similarity.data > 0] = exp_loss[similarity.data > 0] * (S / S1)
+    exp_loss[similarity.data <= 0] = exp_loss[similarity.data <= 0] * (S / S0)
+
+    loss = torch.sum(exp_loss) / S
+
+    #exp_loss = torch.sum(torch.log(1 + exp_product) - similarity * dot_product)
+
+    return loss
+
+
+def pairwise_loss_exam(outputs, label, sigmoid_param=1.0, l_threshold=15.0, class_num=1.0):
+    '''https://github.com/thuml/HashNet/issues/27#issuecomment-494265209'''
+    similarity = Variable(torch.mm(label.data.float(), label.data.float().t()) > 0).float()
+    dot_product = sigmoid_param * torch.mm(outputs, outputs.t())
+    exp_product = torch.exp(dot_product)
+    mask_dot = dot_product.data > l_threshold
+    mask_exp = dot_product.data <= l_threshold
+    mask_positive = similarity.data > 0
+    mask_negative = similarity.data <= 0
+    mask_dp = mask_dot & mask_positive
+    mask_dn = mask_dot & mask_negative
+    mask_ep = mask_exp & mask_positive
+    mask_en = mask_exp & mask_negative
+
+    dot_loss = dot_product * (1-similarity)
+    exp_loss = (torch.log(1+exp_product) - similarity * dot_product)
+    loss = (torch.sum(torch.masked_select(exp_loss, Variable(mask_ep))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dp)))) * class_num + torch.sum(torch.masked_select(exp_loss, Variable(mask_en))) + torch.sum(torch.masked_select(dot_loss, Variable(mask_dn)))
+
+    return loss / (torch.sum(mask_positive.float()) * class_num + torch.sum(mask_negative.float()))
+
+
+def quantization_loss(output):
+    loss = torch.mean((torch.abs(output) - 1) ** 2)
+    return loss
