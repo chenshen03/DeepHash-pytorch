791454e10b3acb7ead74cf47be46e567dbd0cb35
chenshen03 <675121663@qq.com>
2019-06-11 20:03:44 +0800

    无关代码删减

--- train.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python -u train.py --gpu_id 0 --dataset cifar --prefix resnet50_hashnet --hash_bit 48 --net ResNet50 --lr 0.0003 --class_num 1.0
-
-python test.py --gpu_id 0 --dataset cifar --prefix resnet50_hashnet --hash_bit 48 --snapshot iter_09000
